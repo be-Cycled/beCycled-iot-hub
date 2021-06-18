16c6417d2ea091f3fd3a545a4edcde06e019ec3f
Suren Kalaychyan <kalaychyan.s@firstmk.ru>
2021-06-18 22:37:32 +0300

    Добавлен ping инициализатор

---
+++ src/main/java/me/becycled/iothub/server/protocol/ping/PingInitializer.java
@@ -0,0 +1,21 @@
+package me.becycled.iothub.server.protocol.ping;
+
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.handler.codec.string.StringDecoder;
+import io.netty.handler.codec.string.StringEncoder;
+import io.netty.util.CharsetUtil;
+
+/**
+ * @author Suren Kalaychyan
+ */
+public class PingInitializer extends ChannelInitializer<SocketChannel> {
+
+    @Override
+    protected void initChannel(final SocketChannel ch) {
+        ch.pipeline().addLast(
+            new StringDecoder(CharsetUtil.UTF_8),
+            new StringEncoder(CharsetUtil.UTF_8),
+            new PingHandler());
+    }
+}
