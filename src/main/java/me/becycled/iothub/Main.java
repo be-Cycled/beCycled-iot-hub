81ad6418961231c1dd33633fa742a9452bc0b755
Suren Kalaychyan <kalaychyan.s@firstmk.ru>
2021-06-19 00:26:30 +0300

    Обновлен main - метод

--- src/main/java/me/becycled/iothub/Main.java
+++ src/main/java/me/becycled/iothub/Main.java
@@ -1,12 +1,34 @@
 package me.becycled.iothub;
 
+import me.becycled.iothub.server.ServerManager;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
  * @author binakot
  */
 @SuppressWarnings({"HideUtilityClassConstructor", "PMD.UseUtilityClass"})
 public class Main {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(Main.class);
+
     public static void main(final String... args) {
-        // TODO
+        final ServerManager manager = new ServerManager();
+
+        LOGGER.warn("Starting servers...");
+        try {
+            manager.startAll();
+        } catch (Exception ex) {
+            LOGGER.error("Error on servers running up.", ex);
+        }
+
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            LOGGER.warn("Shutting down servers...");
+            try {
+                manager.stopAll();
+            } catch (Exception ex) {
+                LOGGER.error("Error on servers shutting down.", ex);
+            }
+        }));
     }
 }
