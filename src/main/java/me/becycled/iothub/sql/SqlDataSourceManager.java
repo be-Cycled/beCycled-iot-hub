f113bbc6bb6d0af18683a4f80f59a34861f803d1
Ivan Muratov <binakot@gmail.com>
2021-06-19 03:36:50 +0300

    Migrate to props for hikaricp configuration

--- src/main/java/me/becycled/iothub/sql/SqlDataSourceManager.java
+++ src/main/java/me/becycled/iothub/sql/SqlDataSourceManager.java
@@ -4,7 +4,11 @@ import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
 import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
 
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.sql.Connection;
+import java.util.Properties;
 
 /**
  * @author Suren Kalaychyan
@@ -14,20 +18,32 @@ public enum SqlDataSourceManager {
 
     INSTANCE;
 
-    private static final String CONFIG_FILE = "/hikari.properties";
+    private static final String CONFIG_FILE = "src/main/resources/hikari.properties";
 
-    private final HikariConfig config;
     private HikariDataSource dataSource;
-
-    SqlDataSourceManager() {
-        config = new HikariConfig(CONFIG_FILE);
-    }
 
     public Connection getConnection() throws Exception {
         if (dataSource == null) {
+            final Properties props = loadProperties();
+            final HikariConfig config = new HikariConfig(props);
             dataSource = new HikariDataSource(config);
         }
 
         return dataSource.getConnection();
+    }
+
+    @SuppressWarnings("MultipleStringLiterals")
+    private static Properties loadProperties() throws Exception {
+        try (InputStream input = Files.newInputStream(Paths.get(CONFIG_FILE))) {
+            final Properties props = new Properties();
+            props.load(input);
+
+            final String host = System.getProperty("dataSource.serverName", props.getProperty("dataSource.serverName"));
+            final String password = System.getProperty("dataSource.password", props.getProperty("dataSource.password"));
+            props.setProperty("dataSource.serverName", host);
+            props.setProperty("dataSource.password", password);
+
+            return props;
+        }
     }
 }
